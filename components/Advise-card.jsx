0bed882dcb5e9f0cd7242c6fd777953bced54ae8
Sébastien <mrgy.sebastien@gmail.com>
2024-05-08 17:06:20 +0200

    feat(Advise): :iphone:

--- components/Advise-card.jsx
+++ components/Advise-card.jsx
@@ -1,9 +1,11 @@
 import { Rating } from "@mui/material";
 import React from "react";
 
-const AdviseCard = ({ text, rating, name }) => {
+const AdviseCard = ({ text, rating, name, containerStyle }) => {
   return (
-    <article className="max-w-[320px] md:min-w-[320px]  w-full py-9 px-5 bg-white shadow-[0_0_79.4px_-21px_rgba(0,0,0,0.20)]   rounded-[10px] flex flex-col justify-between h-[345px]">
+    <article
+      className={`max-w-[320px] md:min-w-[320px]  w-full py-9 px-5 bg-white shadow-[0_0_79.4px_-21px_rgba(0,0,0,0.20)]   rounded-[10px] flex flex-col justify-between h-[345px] ${containerStyle}`}
+    >
       <span>"</span>
       <p className="text-lg">{text}</p>
 
--- components/Advise.jsx
+++ components/Advise.jsx
@@ -5,9 +5,9 @@ import { AdviseCard } from ".";
 const Advise = () => {
   return (
     <section className="w-full mt-20 ">
-      <div className="bg-[#F5F5F5] px-3.5 md:px-5 pt-10 pb-20 rounded-[10px] md:flex md:flex-row md:items-center ">
-        <div>
-          <h2 className="text-2xl font-bold">
+      <div className="bg-[#F5F5F5] px-3.5 md:px-5 pt-10 pb-20 rounded-[10px] md:flex md:flex-row md:items-center lg:px-16">
+        <div className="">
+          <h2 className="text-2xl font-bold lg:text-5xl ">
             Les indépendants sont satisfaits de leur page de vente
           </h2>
           <p className="font-medium mt-4 md:text-xl">
@@ -21,13 +21,14 @@ const Advise = () => {
           className="mt-4 md:w-2/4 m-auto"
         />
       </div>
-      <div className="max-w-80 m-auto relative -top-10 flex flex-col gap-7 md:flex-row md:max-w-none md:px-5  md:overflow-auto md:py-20 md:-top-36">
+      <div className="max-w-80 m-auto relative -top-10 flex flex-col gap-7 md:flex-row md:max-w-none md:px-5  md:overflow-auto md:py-20 md:-top-36 lg:justify-center lg:gap-16">
         <AdviseCard
           rating={5}
           name={"Hugo G."}
           text={
             "[...] a créé un site web parfait pour mon entreprise, en répondant rapidement à mes besoins et en étant très professionnel."
           }
+          containerStyle={"relative -top-10"}
         />
         <AdviseCard
           rating={5}
@@ -35,6 +36,7 @@ const Advise = () => {
           text={
             "Il a su contribuer à mes projets en proposant des idées pertinentes."
           }
+          containerStyle={"relative top-10"}
         />
         <AdviseCard
           rating={5}
