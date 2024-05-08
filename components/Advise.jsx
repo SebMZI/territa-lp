85a5a0486cacad47165fa4abc48039e7b2bc6e98
Sébastien <mrgy.sebastien@gmail.com>
2024-05-08 12:57:42 +0200

    feat(Advise): :iphone: Card are now aligned horizontally and text is bigger
    Changed the disposition to row, font sizes are bigger. Still need to figure out why shadows are not workig when overflow is on scroll

--- components/Advise.jsx
+++ components/Advise.jsx
@@ -4,22 +4,24 @@ import { AdviseCard } from ".";
 
 const Advise = () => {
   return (
-    <section className="w-full mt-20">
-      <div className="bg-[#F5F5F5] px-3.5 pt-10 pb-20 rounded-[10px]">
-        <h2 className="text-2xl font-bold">
-          Les indépendants sont satisfaits de leur page de vente
-        </h2>
-        <p className="font-medium mt-4">
-          Ne nous crois pas sur parole, écoutes ce qu’ils ont à dire.
-        </p>
+    <section className="w-full mt-20 ">
+      <div className="bg-[#F5F5F5] px-3.5 md:px-5 pt-10 pb-20 rounded-[10px] md:flex md:flex-row md:items-center">
+        <div>
+          <h2 className="text-2xl font-bold">
+            Les indépendants sont satisfaits de leur page de vente
+          </h2>
+          <p className="font-medium mt-4 md:text-xl">
+            Ne nous crois pas sur parole, écoutes ce qu’ils ont à dire.
+          </p>
+        </div>
         <Image
           src={"/images/advise-icon.svg"}
           width={339}
           height={289}
-          className="mt-4"
+          className="mt-4 md:w-2/4"
         />
       </div>
-      <div className="max-w-80 m-auto relative -top-10 flex flex-col gap-7">
+      <div className="max-w-80 m-auto relative -top-10 flex flex-col gap-7 md:flex-row md:max-w-none md:px-5  md:overflow-x-scroll md:h-">
         <AdviseCard
           rating={5}
           name={"Hugo G."}
