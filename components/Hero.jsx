a235256a357ef4d398e6d6b614a53c5849c315a3
Sébastien <mrgy.sebastien@gmail.com>
2024-05-08 16:22:42 +0200

    feat(Hero): :iphone: Desktop responsive
    title bigger

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,15 +11,15 @@ const Hero = () => {
           pages de vente sur-mesure pour les indépendants
         </span>
       </div>
-      <h1 className="font-bold text-center text-[28px] leading-9 mt-4 mb-3 md:text-4xl">
+      <h1 className="font-bold text-center text-[28px] leading-9 mt-4 mb-3 md:text-4xl lg:w-3/4 lg:m-auto lg:my-4 lg:text-5xl">
         <span className="text-ascent">Augmente tes conversions</span> grâce à
         une page de vente authentique et personnalisée
       </h1>
-      <p className="text-sm text-center mt-2 font-medium md:text-base">
+      <p className="text-sm text-center mt-2 font-medium md:text-base lg:text-xl">
         Construisons ensemble la page de vente qui te colle à la peau.
       </p>
 
-      <ul className="mt-7 flex flex-col gap-3 mb-7 md:flex-row md:justify-between">
+      <ul className="mt-7 flex flex-col gap-3 mb-7 md:flex-row md:justify-around lg:px-10">
         <li className="flex flex-row gap-5 items-center">
           <Image
             src={"/images/green-check.png"}
@@ -65,11 +65,11 @@ const Hero = () => {
       </div>
 
       <div className="flex justify-center relative mt-10">
-        <div className="shadow-info h-[44px] w-[99px] text-center absolute top-2 md:-top-4 left-0 md:left-7 md:px-4 md:h-auto md:w-auto md:py-1  bg-white rounded-[10px] z-10 flex flex-col items-center justify-center">
+        <div className="shadow-info h-[44px] w-[99px] text-center absolute top-2 md:-top-4 left-0 md:left-7 md:px-4 md:h-auto md:w-auto md:py-1  bg-white rounded-[10px] z-10 flex flex-col items-center justify-center lg:left-36">
           <h2 className="text-lg text-ascent md:text-3xl">+ 10%</h2>
           <p className="text-[8px] md:text-sm">taux de conversion</p>
         </div>
-        <div className="shadow-info h-[44px] w-[99px] text-center absolute top-6 right-3 md:right-7 md:top-20 bg-white rounded-[10px] z-10 flex flex-col items-center justify-center md:px-8 md:h-auto md:w-auto md:py-1 ">
+        <div className="shadow-info h-[44px] w-[99px] text-center absolute top-6 right-3 md:right-7 md:top-20 bg-white rounded-[10px] z-10 flex flex-col items-center justify-center md:px-8 md:h-auto md:w-auto md:py-1 lg:right-24 lg:top-36">
           <h2 className="text-lg text-ascent md:text-3xl">3 écrans</h2>
           <p className="text-[8px] md:text-sm">responsive</p>
         </div>
