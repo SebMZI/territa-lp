e836548e64b29dd5fe4e4de1e0030ca611699264
Sébastien <mrgy.sebastien@gmail.com>
2024-05-08 12:33:01 +0200

    feat(LastCTA): :iphone: Add advise card in responsive tablet
    The advise appears when the user is on a Tablet and disappear if on mobile

--- components/LastCTA.jsx
+++ components/LastCTA.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Button from "./Button";
+import AdviseCard from "./Advise-card";
 
 const LastCTA = () => {
   return (
-    <section className="flex flex-col items-center gap-5 mt-20">
-      <h2 className="text-center text-3xl font-bold">
-        Prêt à convertir tes visiteurs en clients ?
-      </h2>
-      <Button containerStyle={" text-sm h-[45px] px-10"} />
+    <section className=" mt-20 md:flex md:flex-row md:justify-between md:items-center ">
+      <div className="flex flex-col items-center gap-5 md:items-start md:w-2/4">
+        <h2 className="text-center text-3xl font-bold md:text-left md:text-4xl">
+          Prêt à convertir tes visiteurs en clients ?
+        </h2>
+        <Button containerStyle={" text-sm h-[45px] px-10"} />
+      </div>
+      <div className="hidden md:block">
+        <AdviseCard
+          text={
+            "Il a parfaitement compris mes besoins et a livré un site web qui dépasse mes attentes."
+          }
+          rating={5}
+          name={"Moulay E."}
+        />
+      </div>
     </section>
   );
 };
