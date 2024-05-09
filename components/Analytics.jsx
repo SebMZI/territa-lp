39ecced6e2ab1173460de303e9a5ba49b58eb096
Sébastien <mrgy.sebastien@gmail.com>
2024-05-09 14:06:43 +0200

    feat:

--- components/Analytics.jsx
+++ components/Analytics.jsx
@@ -5,72 +5,76 @@ const Analytics = () => {
   return (
     <>
       {/* Scheme.org */}
-      <Script type="application/ld+json">
-        {`
-          "@context": "https://schema.org/", 
-          "@type": "Product", 
-          "name": "Page de vente sur-mesure",
-          "image": "https://services.territa.fr/opengraph.png",
-          "description": "Augmenter la conversion des visiteurs en leads qualifiés avec une page de vente authentique et sur-mesure.",
-          "brand": {
-            "@type": "Brand",
-            "name": "Territa"
-          },
-          "offers": {
-            "@type": "Offer",
-            "url": "https://services.territa.fr",
-            "priceCurrency": "EUR",
-            "price": "397",
-            "availability": "https://schema.org/OnlineOnly",
-            "itemCondition": "https://schema.org/NewCondition"
-          },
-          "aggregateRating": {
-            "@type": "AggregateRating",
-            "ratingValue": "5",
-            "bestRating": "5",
-            "worstRating": "5",
-            "ratingCount": "3",
-            "reviewCount": "3"
-          },
-          "review": [{
-            "@type": "Review",
-            "name": "Moulay E.",
-            "reviewBody": "Je suis extrêmement satisfait du travail de Sébastien. Son professionnalisme et son expertise ont été remarquables. Il a parfaitement compris mes besoins et a livré un site web qui dépasse mes attentes. Je recommande vivement ses services",
-            "reviewRating": {
-              "@type": "Rating",
-              "ratingValue": "5",
-              "bestRating": "5",
-              "worstRating": "5"
-            },
-            "datePublished": "2024-03-11",
-            "author": {"@type": "Person", "name": "Moulay"}
-          },{
-            "@type": "Review",
-            "name": "Damien D.",
-            "reviewBody": "Le savoir faire de Sébastien a été à la hauteur de mes attentes concernant le design de maquette web. Il a su contribuer à mes projets en proposant des idées pertinentes. Je recommande vivement ses prestations !",
-            "reviewRating": {
-              "@type": "Rating",
-              "ratingValue": "5",
-              "bestRating": "5",
-              "worstRating": "5"
-            },
-            "datePublished": "2024-03-14",
-            "author": {"@type": "Person", "name": "Damien D."}
-          },{
-            "@type": "Review",
-            "name": "Hugo G.",
-            "reviewBody": "Excellent travail ! Sebastien a créé un site web parfait pour mon entreprise, en répondant rapidement à mes besoins et en étant très professionnel. Je le recommande vivement !",
-            "reviewRating": {
-              "@type": "Rating",
-              "ratingValue": "5",
-              "bestRating": "5",
-              "worstRating": "5"
-            },
-            "datePublished": "2024-03-04",
-            "author": {"@type": "Person", "name": "Hugo"}
-          }]
-        `}
-      </Script>
+      <Script
+        id="metadata"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: ` {
+              "@context": "https://schema.org/", 
+              "@type": "Product", 
+              "name": "Page de vente sur-mesure",
+              "image": "https://services.territa.fr/opengraph.png",
+              "description": "Augmenter la conversion des visiteurs en leads qualifiés avec une page de vente authentique et sur-mesure.",
+              "brand": {
+                "@type": "Brand",
+                "name": "Territa"
+              },
+              "offers": {
+                "@type": "Offer",
+                "url": "https://services.territa.fr",
+                "priceCurrency": "EUR",
+                "price": "397",
+                "availability": "https://schema.org/OnlineOnly",
+                "itemCondition": "https://schema.org/NewCondition"
+              },
+              "aggregateRating": {
+                "@type": "AggregateRating",
+                "ratingValue": "5",
+                "bestRating": "5",
+                "worstRating": "5",
+                "ratingCount": "3",
+                "reviewCount": "3"
+              },
+              "review": [{
+                "@type": "Review",
+                "name": "Moulay E.",
+                "reviewBody": "Je suis extrêmement satisfait du travail de Sébastien. Son professionnalisme et son expertise ont été remarquables. Il a parfaitement compris mes besoins et a livré un site web qui dépasse mes attentes. Je recommande vivement ses services",
+                "reviewRating": {
+                  "@type": "Rating",
+                  "ratingValue": "5",
+                  "bestRating": "5",
+                  "worstRating": "5"
+                },
+                "datePublished": "2024-03-11",
+                "author": {"@type": "Person", "name": "Moulay"}
+              },{
+                "@type": "Review",
+                "name": "Damien D.",
+                "reviewBody": "Le savoir faire de Sébastien a été à la hauteur de mes attentes concernant le design de maquette web. Il a su contribuer à mes projets en proposant des idées pertinentes. Je recommande vivement ses prestations !",
+                "reviewRating": {
+                  "@type": "Rating",
+                  "ratingValue": "5",
+                  "bestRating": "5",
+                  "worstRating": "5"
+                },
+                "datePublished": "2024-03-14",
+                "author": {"@type": "Person", "name": "Damien D."}
+              },{
+                "@type": "Review",
+                "name": "Hugo G.",
+                "reviewBody": "Excellent travail ! Sebastien a créé un site web parfait pour mon entreprise, en répondant rapidement à mes besoins et en étant très professionnel. Je le recommande vivement !",
+                "reviewRating": {
+                  "@type": "Rating",
+                  "ratingValue": "5",
+                  "bestRating": "5",
+                  "worstRating": "5"
+                },
+                "datePublished": "2024-03-04",
+                "author": {"@type": "Person", "name": "Hugo"}
+              }]
+            }`,
+        }}
+      />
 
       {/* Google tag (gtag.js) */}
       <Script
